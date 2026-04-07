9b510882273697f5036bb4f93d2a3ebf4715d54f
GrannyApples <anton.engstrom1994@gmail.com>
2026-04-07 16:32:12 +0200

    feat: add initial transform

--- src/etl/extract.py
+++ src/etl/extract.py
@@ -4,5 +4,11 @@ class Extract:
     def __init__(self):
         self.service = TMDBService()
 
-    def fetch_movies(self):
-        return self.service.get_fantasy_movies()
+    def fetch_movies(self, pages=5):
+        all_movies = []
+
+        for page in range(1, pages + 1):
+            data = self.service.get_fantasy_movies(page=page)
+            all_movies.extend(data)
+
+        return all_movies
